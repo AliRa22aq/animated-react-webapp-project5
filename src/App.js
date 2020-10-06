97c9072c9be3a070a3df96ac3e282324575d04b1
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-06 23:51:44 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home'
 import Extra from './components/Extra'
 import NotFound from './components/NotFound'

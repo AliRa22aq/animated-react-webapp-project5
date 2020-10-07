71147781b0c5a21f4525781dad01f57b1e935892
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-07 16:51:06 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Grid} from "@material-ui/core"
-import { Routes, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (

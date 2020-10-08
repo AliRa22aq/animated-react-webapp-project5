dc7e31857b714c68120992c505d4a2c249fabc19
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-08 07:25:27 +0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { motion, useAnimation } from "framer-motion";
+
+const variants = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  }
 
 function Home() {
     return (
-        <div>
-            Hellow Home
+        <div className = 'homeBG'>
+            <motion.div initial="hidden" animate="visible" variants={variants}>
+
+            Hello Home
+
+            </motion.div>
         </div>
     )
 }

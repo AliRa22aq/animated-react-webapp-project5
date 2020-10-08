1aac55a950e64f551879d9ebcd0cffa99b0879a1
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-08 07:25:20 +0500

    Update More.js

--- src/components/More.js
+++ src/components/More.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { motion } from "framer-motion";
+import { AutoRotatingCarousel, Slide } from 'material-auto-rotating-carousel';
+
+
+const variants = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  }
 
 function More() {
+
     return (
-        <div>
-            I am More
+        <div className='moreBG'>
+                        <motion.div initial="hidden" animate="visible" variants={variants}>
+                            I am more
+                        </motion.div>
+
+
+
+
+
         </div>
     )
 }
 
-export default More
+export default More;

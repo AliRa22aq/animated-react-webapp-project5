a07f06acd5e0a3344691aa1a8dc49e19b219f0e0
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-08 09:51:51 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Grid} from "@material-ui/core"
 import { Link } from 'react-router-dom';
+import { motion } from "framer-motion";
+
 
 function Header() {
     return (
@@ -8,19 +10,44 @@ function Header() {
         <Grid container>
             <Grid item xs={2} > <Link to="/"> 
             <img 
-            src=""
+            src="https://i2.wp.com/freepngimages.com/wp-content/uploads/2016/10/harrods-logo.png?fit=895%2C543"
             alt = "Logo"
-            height="20px"
-            width="20px"
+            height="70px"
+            width="110px"
             /> </Link> </Grid> 
             <Grid item xs={6} > </Grid> 
-            <Grid item xs={1} className = "menu-btn"> <Link to="/"> Home </Link> </Grid> 
-            <Grid item xs={1} className = "menu-btn"> <Link to='/Extra'> Extra </Link> </Grid> 
-            <Grid item xs={1} className = "menu-btn"> <Link to='/More'> More </Link> </Grid> 
-            <Grid item xs={1} className = "menu-btn"> <Link to='/Contact'> Contact </Link> </Grid> 
+            
+                <Grid item xs={1} >
+                  <Link to="/"> 
+                    <motion.button className= 'button' whileHover={{ scale: 1.2, transition: { duration: 0.3 } }} whileTap={{ scale: 0.9 }}>
+                    Home 
+                    </motion.button>
+                  </Link>
+                </Grid> 
 
-
-
+                <Grid item xs={1} >
+                    <Link to="/Extra">
+                        <motion.button className='button' whileHover={{ scale: 1.2, transition: { duration: 0.3 } }} whileTap={{ scale: 0.9 }}>
+                            Services
+                        </motion.button>
+                    </Link>
+                </Grid> 
+                
+                <Grid item xs={1} >
+                    <Link to='/More'> 
+                        <motion.button className='button' whileHover={{ scale: 1.2, transition: { duration: 0.3 } }} whileTap={{ scale: 0.9 }}>
+                            More
+                        </motion.button>
+                    </Link>
+                </Grid> 
+               
+                <Grid item xs={1} >
+                    <Link to='/Contact'>
+                        <motion.button className= 'button' whileHover={{ scale: 1.2, transition: { duration: 0.3 } }} whileTap={{ scale: 0.9 }}>
+                            Contact 
+                        </motion.button>
+                    </Link> 
+                </Grid> 
 
         </Grid>
             

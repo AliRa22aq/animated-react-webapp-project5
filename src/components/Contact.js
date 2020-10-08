0ee5be006e618a82c8c3cb3d45bb6da057162171
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-08 07:25:10 +0500

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { motion } from "framer-motion";
+
+
+const variants = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  }
 
 function Contact() {
     return (
-        <div>
-            Hello Contact
+        <div className='moreBG'>
+
+            <motion.div initial="hidden" animate="visible" variants={variants}>
+                
+                I am Contact
+            
+            </motion.div>
         </div>
     )
 }

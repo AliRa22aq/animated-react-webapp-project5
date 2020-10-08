409742569f0a7258daf7d3858df5afb80df2a3a4
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-08 07:25:15 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,13 @@ function Header() {
     return (
         <div class='header' >
         <Grid container>
-            <Grid item xs={2} > <Link to="/"> LOGO </Link> </Grid> 
+            <Grid item xs={2} > <Link to="/"> 
+            <img 
+            src=""
+            alt = "Logo"
+            height="20px"
+            width="20px"
+            /> </Link> </Grid> 
             <Grid item xs={6} > </Grid> 
             <Grid item xs={1} className = "menu-btn"> <Link to="/"> Home </Link> </Grid> 
             <Grid item xs={1} className = "menu-btn"> <Link to='/Extra'> Extra </Link> </Grid> 

a9e970d93831ab8f79987916fb42c39b1ea5b434
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-10 03:09:45 +0500

    Update More.js

--- src/components/More.js
+++ src/components/More.js
@@ -1,19 +1,24 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import { motion } from "framer-motion";
+
+
+
 
 
 const variants = {
     visible: { opacity: 1 },
     hidden: { opacity: 0 },
   }
-
+   
 function More() {
+
 
     return (
         <div className='moreBG'>
-                        <motion.div initial="hidden" animate="visible" variants={variants}>
-                            I am more
-                        </motion.div>
+        <motion.div initial="hidden" animate="visible" variants={variants}>
+                I am more
+        </motion.div>
+
 
         </div>
     )
